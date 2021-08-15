f32b343c42ff98f577caf73082ca682a4a9c9b08
Christian <christiancosta.parreiras@yahoo.com.br>
2021-08-15 14:55:25 -0300

    Adicionada permissao geral para metodos get na rota de topicos.

--- src/main/java/br/com/alura/forumcurso/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forumcurso/config/security/SecurityConfigurations.java
@@ -2,6 +2,10 @@ package br.com.alura.forumcurso.config.security;
 
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,6 +13,24 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
+    // Serve para configurar a parte de Autenticação.
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
 
+    // Configurações de Autorização.
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+        super.configure(http);
+    }
 
+    // Configurações de recursos estáticos (js, css, imagens etc.)
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+    }
 }
